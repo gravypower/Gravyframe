f404adc61aaaec5fcc0904d7578f5bc71019ae13
gravypower <awjob42@gmail.com>
2013-09-23 18:29:27 +0100

    Fixed up the example service stack application with the new changes

--- Src/Gravyframe.ServiceStack.Content/ContentAppHost.cs
+++ Src/Gravyframe.ServiceStack.Content/ContentAppHost.cs
@@ -1,8 +1,11 @@
 ﻿using Funq;
 using Gravyframe.Data.Content;
 using Gravyframe.Data.InMemory.Content;
+using Gravyframe.Service;
 using Gravyframe.Service.Content;
+using Gravyframe.Service.Content.Tasks;
 using ServiceStack.WebHost.Endpoints;
+using System.Collections.Generic;
 
 namespace Gravyframe.ServiceStack.Content
 {
@@ -15,8 +18,17 @@ namespace Gravyframe.ServiceStack.Content
         public override void Configure(Container container)
         {
             container.Register<IContentDao>(dao => new InMemoryContentDao());
-            container.Register<IContentConstants>(dao => new ContentConstants());
+            container.Register<IContentConstants>(constants => new ContentConstants());
 
+            container.Register<IEnumerable<Task<ContentRequest, ContentResponse>>>(tasks => 
+                new List<Task<ContentRequest, ContentResponse>>
+                    {
+                        new PopulateContentByCategoryIdTask(container.Resolve<IContentDao>(), container.Resolve<IContentConstants>()),
+                        new PopulateContentByIdTask(container.Resolve<IContentDao>(), container.Resolve<IContentConstants>())
+                    }
+                );
+
+            Routes.Add<ContentRequest>("/ContentService/");
             Routes.Add<ContentRequest>("/ContentService/{ContentId}");
             Routes.Add<ContentRequest>("/ContentService/Category/{CategoryId}");
         }
