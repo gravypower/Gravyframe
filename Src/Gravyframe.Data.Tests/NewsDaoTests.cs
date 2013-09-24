4919babada817a5da723da733528d5706cbd6092
gravypower <awjob42@gmail.com>
2013-09-24 08:46:26 +0100

    Started to add some tests for NewsDao

---
+++ Src/Gravyframe.Data.Tests/NewsDaoTests.cs
@@ -0,0 +1,18 @@
+﻿using Gravyframe.Data.News;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Gravyframe.Data.Tests
+{
+    [TestFixture]
+    public class NewsDaoTests
+    {
+        public INewsDao Sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Sut = Substitute.For<NewsDao>();
+        }
+    }
+}
---
+++ Src/Gravyframe.Data/News/NewsDao.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Gravyframe.Data.News
+{
+    public abstract class NewsDao : INewsDao
+    {
+        public abstract Models.News GetNews(string newsId);
+
+        public abstract IEnumerable<Models.News> GetNewsByCategoryId(string categoryId);
+    }
+}
