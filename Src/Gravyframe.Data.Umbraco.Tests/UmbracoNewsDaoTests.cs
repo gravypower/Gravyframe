1bafbddec92321e006dd9ef4035e761a08624b42
gravypower <awjob42@gmail.com>
2013-10-07 21:49:50 +0100

    Playing around with Umbraco implementation

---
+++ Src/Gravyframe.Data.Umbraco.Tests/UmbracoNewsDaoTests.cs
@@ -0,0 +1,15 @@
+﻿using Gravyframe.Data.Tests;
+using Gravyframe.Data.Umbraco.News;
+using NUnit.Framework;
+
+namespace Gravyframe.Data.Umbraco.Tests
+{
+    public class UmbracoNewsDaoTests : NewsDaoTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Sut = new UmbracoNewsDao();
+        }
+    }
+}
