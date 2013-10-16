7063dd0359bda056ad8444ccf0bb1edfc0e319c7
gravypower <awjob42@gmail.com>
2013-10-16 21:19:33 +0100

    still playing with mocking lucene

--- Src/Gravyframe.Data.Umbraco.Tests/UmbracoNewsDaoTests.cs
+++ Src/Gravyframe.Data.Umbraco.Tests/UmbracoNewsDaoTests.cs
@@ -4,15 +4,16 @@ using Gravyframe.Data.Tests;
 using Gravyframe.Data.Umbraco.News;
 using Gravyframe.Kernel.Umbraco;
 using Gravyframe.Kernel.Umbraco.Tests;
+using Lucene.Net.Documents;
 using NSubstitute;
 using NUnit.Framework;
+using UmbracoExamine;
 using umbraco.interfaces;
 
 namespace Gravyframe.Data.Umbraco.Tests
 {
     using Lucene.Net.Analysis;
     using Lucene.Net.Index;
-    using Lucene.Net.Search;
     using Lucene.Net.Store;
 
     [TestFixture]
@@ -48,25 +49,29 @@ namespace Gravyframe.Data.Umbraco.Tests
         [Test]
         public void SomeOtherTEst()
         {
-            var document = new Lucene.Net.Documents.Document();
+            var document = new Document();
+            var feild = Substitute.For<Fieldable>();
+            feild.Name().Returns("nodeName");
+            feild.StringValue().Returns("Hello");
+ 
+            document.Add(feild);
             var directory = new RAMDirectory();
             var analyzer = new SimpleAnalyzer();
-            var writer = new IndexWriter(directory, analyzer, true);
+            var writer = new IndexWriter(directory, analyzer, true, new IndexWriter.MaxFieldLength(10));
 
             writer.AddDocument(document);
 
             writer.Close();
 
-
             var reader = IndexReader.Open(directory, true);
-            var searcher = new IndexSearcher(reader);
 
-            //searcher.Search()
+            var searcher = new UmbracoExamineSearcher(directory, new KeywordAnalyzer());
+            var searchCriteria = searcher.CreateSearchCriteria();
+            var query = searchCriteria.Field("nodeName", "hello").Compile();
+            var searchResults = searcher.Search(query);
 
             reader.Close();
-            searcher.Close();
-
-
+            Assert.IsTrue(searchResults.TotalItemCount > 0);
         }
 
         protected override string GetExampleId()
