00179e67a32cd38da6e6ffd73c747dba63f2feb4
gravypower <aaron@gravypower.net>
2013-11-04 17:04:17 +0000

    Playing around with the umbraco example
    Changed the project type for Gravyframe.ServiceStack.Umbraco
    Added in a masterpage

--- Src/Gravyframe.ServiceStack.Umbraco/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gravyframe.ServiceStack.Umbraco.Global" Language="C#" %>
---
+++ Src/Gravyframe.UI.CMS.Umbraco/masterpages/Gravyframe.Master.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gravyframe.UI.CMS.Umbraco.masterpages
+{
+    public partial class Gravyframe : global::Umbraco.Web.UI.Umbraco.Masterpages.Default
+    {
+    }
+}
